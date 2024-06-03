892daac5ab8297df6eb9284e6927ecc922cfdabb
Jon <jharder@kipsu.com>
2024-06-03 13:01:53 -0500

    Initial ferris/sweep hand down vibranium layout

---
+++ keyboards/ferris/keymaps/hd-vibranium/config.h
@@ -0,0 +1,58 @@
+/*
+Copyright 2020 Pierre Chevalier <pierrechevalier83@gmail.com>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+// Set the mouse settings to a comfortable speed/accuracy trade-off,
+// assuming a screen refresh rate of 60 Htz or higher
+// The default is 50. This makes the mouse ~3 times faster and more accurate
+#define MOUSEKEY_INTERVAL 16
+// The default is 20. Since we made the mouse about 3 times faster with the previous setting,
+// give it more time to accelerate to max speed to retain precise control over short distances.
+#define MOUSEKEY_TIME_TO_MAX 40
+// The default is 300. Let's try and make this as low as possible while keeping the cursor responsive
+#define MOUSEKEY_DELAY 100
+// It makes sense to use the same delay for the mouseweel
+#define MOUSEKEY_WHEEL_DELAY 100
+// The default is 100
+#define MOUSEKEY_WHEEL_INTERVAL 50
+// The default is 40
+#define MOUSEKEY_WHEEL_TIME_TO_MAX 100
+
+// Pick good defaults for enabling homerow modifiers
+#define TAPPING_TERM 200
+#define PERMISSIVE_HOLD
+#define QUICK_TAP_TERM 0
+
+#define ADAPTIVE_TERM 65
+
+#define EE_HANDS
+
+/* #undef DIRECT_PINS_RIGHT */
+/* #define DIRECT_PINS_RIGHT { \ */
+/*     { D7, F7, F6, F5, F4 }, \ */
+/*     { B1, B3, B2, B6, D3 }, \ */
+/*     { D1, D0, D4, C6, E6 }, \ */
+/*     { B4, B5, NO_PIN, NO_PIN, NO_PIN } \ */
+/* } */
+/* #undef DIRECT_PINS */
+/* #define DIRECT_PINS { \ */
+/*     { F4, F5, F6, F7, D7 }, \ */
+/*     { D3, B6, B2, B3, B1 }, \ */
+/*     { E6, C6, D4, D0, D1 }, \ */
+/*     { B5, B4, NO_PIN, NO_PIN, NO_PIN } \ */
+/* } */
+
