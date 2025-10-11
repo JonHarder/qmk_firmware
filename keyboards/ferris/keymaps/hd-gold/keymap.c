76d68c7c3f37311cbe2c4e785e259e0278f60d96
Jon <jharder@kipsu.com>
2025-10-11 14:30:25 -0500

    Use regular number keys, not numpad

--- keyboards/ferris/keymaps/hd-gold/keymap.c
+++ keyboards/ferris/keymaps/hd-gold/keymap.c
@@ -194,9 +194,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                       _______, KC_SPC,    _______,  _______
    ), /*                                                            LA_SYM                            */
    [_NUM] = LAYOUT_split_3x5_2(
-       KC_TRNS,   KC_PERC, KC_EQL,    KC_TRNS, KC_TRNS,   KC_TRNS,  KC_P7,   KC_P8,    KC_P9,     KC_EQL,
-       OS_CTRL,   OS_OPT,  OS_CMD,    OS_SHFT, KC_TRNS,   KC_TRNS,  KC_P4,   KC_P5,    KC_P6,     KC_BSPC,
-       QK_BOOT,   KC_PDOT, KC_MINS,   KC_COLN, KC_TRNS,   KC_TRNS,  KC_P1,   KC_P2,    KC_P3,     KC_P0,
+       KC_TRNS,   KC_PERC, KC_EQL,    KC_TRNS, KC_TRNS,   KC_TRNS,  KC_7,   KC_8,    KC_9,     KC_EQL,
+       OS_CTRL,   OS_OPT,  OS_CMD,    OS_SHFT, KC_TRNS,   KC_TRNS,  KC_4,   KC_5,    KC_6,     KC_BSPC,
+       QK_BOOT,   KC_PDOT, KC_MINS,   KC_COLN, KC_TRNS,   KC_TRNS,  KC_1,   KC_2,    KC_3,     KC_0,
                                       _______, _______,   _______,  _______
    ), /*                              LA_NAV                        LA_SYM                            */
    [_OSH] = LAYOUT(/* [> One Shot Layer <] */
